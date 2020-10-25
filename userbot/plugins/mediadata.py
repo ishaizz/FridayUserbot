ca067bf17cd0c25c37bf82bbd7dad6add70ac38f
StarkGang <StarkGangZ@gmail.com>
2020-10-25 21:25:13 +0530

    Update mediadata.py

--- userbot/plugins/mediadata.py
+++ userbot/plugins/mediadata.py
@@ -6,7 +6,7 @@ from userbot.uniborgConfig import Config
 from userbot.utils import friday_on_cmd, sudo_cmd
 from MediaInfo import MediaInfo
 from telegraph import Telegraph, exceptions, upload_file
-
+import telegraph 
 @friday.on(friday_on_cmd(pattern="mediainfo (.*)"))  # pylint:disable=E0602
 async def _(event):
     if event.fwd_from:
