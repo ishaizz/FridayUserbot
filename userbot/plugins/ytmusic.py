60a4607707097afbd60d9143245727e12a3b98d6
StarkGang <StarkGangZ@gmail.com>
2020-10-25 16:49:17 +0530

    Update ytmusic.py

--- userbot/plugins/ytmusic.py
+++ userbot/plugins/ytmusic.py
@@ -25,17 +25,14 @@ async def _(event):
     youtube_video_url = f"{mo}"
     yt_obj = YouTube(youtube_video_url)
     await asyncio.sleep(0.6)
-    if not os.path.isdir(Config.TMP_DOWNLOAD_DIRECTORY):
-        os.makedirs(Config.TMP_DOWNLOAD_DIRECTORY)
+    if not os.path.isdir('./music/'):
+        os.makedirs('./music/')
     path = Config.TMP_DOWNLOAD_DIRECTORY
     sedlyf = Download(kekme).download()
-    keks = yt_obj.streams.get_audio_only().download(output_path=path, filename=f'{thum}')
+    stark = f'youtube-dl -q --metadata-from-title -o "./music/%(title)s.%(ext)s" --extract-audio --audio-format mp3 --audio-quality 128k {mo}'
+    os.system(stark)
     await myself_stark.edit("`Song Downloaded Sucessfully. Let Me Upload it Here.`")
-    renamee = keks
-    pre, ext = os.path.splitext(renamee)
-    new_extension = ".mp3"
-    hmm = os.rename(renamee, pre + new_extension)
-    km = pre + new_extension
+    km = f"./music/{thum}.mp3"
     capy = f"**Song Name ➠** `{thum}` \n**Requested For ➠** `{urlissed}` \n**Channel ➠** `{thums}` \n**Link ➠** `{mo}`" 
     await borg.send_file(event.chat_id,
                 km,
